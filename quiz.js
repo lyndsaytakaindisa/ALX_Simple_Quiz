d6979ba4ba1b005302431ace3c077d77315b725f
Lyndsay Takaindisa <lyndsaytakaindisa@gmail.com>
2025-01-22 22:44:51 +0200

    Implementation of an interactive quiz

--- quiz.js
+++ quiz.js
@@ -1,6 +1,7 @@
 function checkAnswer(){ // function declaraction
     let correctAnswer = "4"; // identify the correct answer
-    let userAnswer    =document.querySelector("input[name='quiz']:checked"); // retrieve the user's answer 
+    let answer   =document.querySelector("input[name='quiz']:checked"); // retrieve the user's answer 
+    userAnswer = answer.value ;
     if (userAnswer === correctAnswer){ // copmare the user's answer with the correct answer 
         document.getElementById("feedback").textContent="Correct! Well done." 
     }
